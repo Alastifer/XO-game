b0104408b965a8dde26ef7ca6e010f0d59535309
alastifer <grishkevichaaa@gmail.com>
2016-08-10 12:57:49 +0300

    Player - completed

---
+++ src/project/xo/model/Player.java
@@ -0,0 +1,21 @@
+package project.xo.model;
+
+public class Player {
+
+    private final String name;
+
+    private final Figure playerFigure;
+
+    public Player(final String name, final Figure playerFigure) {
+        this.name = name;
+        this.playerFigure = playerFigure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Figure getPlayerFigure() {
+        return playerFigure;
+    }
+}
