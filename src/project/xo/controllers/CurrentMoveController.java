2330c634c550f86ca1a11219888a2a5b8f0ba1e3
alastifer <grishkevichaaa@gmail.com>
2016-08-11 09:25:25 +0300

    CurrentMoveController - created

---
+++ src/project/xo/controllers/CurrentMoveController.java
@@ -0,0 +1,39 @@
+package project.xo.controllers;
+
+import project.xo.model.Field;
+import project.xo.model.Figure;
+import project.xo.model.Point;
+
+public class CurrentMoveController {
+
+    public Figure currentMove(final Field field) {
+        final int numberOfFigure = moveRow(field);
+
+        if (numberOfFigure % 2 == 0) {
+            return Figure.X;
+        } else {
+            return Figure.O;
+        }
+    }
+
+    private int moveRow(final Field field) {
+        int numberOfFigure = 0;
+
+        for (int i = 0; i < field.getSize(); i++) {
+            numberOfFigure = moveColumn(field, i, numberOfFigure);
+        }
+
+        return numberOfFigure;
+    }
+
+    private int moveColumn(final Field field, final int row, int numberOfFigure) {
+        for (int i = 0; i < field.getSize(); i++) {
+            if (field.getFigure(new Point(i, row)) != null) {
+                numberOfFigure++;
+            }
+        }
+
+        return numberOfFigure;
+    }
+
+}
