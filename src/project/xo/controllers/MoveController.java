c26b4bc135cff5adab4e9a965dd3d3e80ca8bab4
alastifer <grishkevichaaa@gmail.com>
2016-08-12 15:14:59 +0300

    add throw AlreadyOccupiedException

--- src/project/xo/controllers/MoveController.java
+++ src/project/xo/controllers/MoveController.java
@@ -1,14 +1,15 @@
 package project.xo.controllers;
 
+import project.xo.controllers.exceptions.AlreadyOccupiedException;
 import project.xo.model.Field;
 import project.xo.model.Figure;
 import project.xo.model.Point;
 
 public class MoveController {
 
-    public void applyFigure(final Field field, final Point point, final Figure figure) {
+    public void applyFigure(final Field field, final Point point, final Figure figure) throws AlreadyOccupiedException {
         if (field.getFigure(point) != null) {
-            //throw exception
+            throw new AlreadyOccupiedException();
         }
 
         field.setFigure(figure, point);
