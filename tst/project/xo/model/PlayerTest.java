9e31920b773fbcba45a90a17094a2391b30ff0f0
alastifer <grishkevichaaa@gmail.com>
2016-08-12 11:41:29 +0300

    fix PlayerTest

--- tst/project/xo/model/PlayerTest.java
+++ tst/project/xo/model/PlayerTest.java
@@ -7,16 +7,16 @@ import static org.junit.Assert.*;
 public class PlayerTest {
     @Test
     public void getName() throws Exception {
-        String trueName = "Alex";
-        Player player = new Player(trueName, null);
+        final String trueName = "Alex";
+        final Player player = new Player(trueName, null);
 
         assertEquals(trueName, player.getName());
     }
 
     @Test
     public void getPlayerFigure() throws Exception {
-        Figure trueFigure = Figure.O;
-        Player player = new Player(null, trueFigure);
+        final Figure trueFigure = Figure.O;
+        final Player player = new Player(null, trueFigure);
 
         assertEquals(trueFigure, player.getPlayerFigure());
     }
