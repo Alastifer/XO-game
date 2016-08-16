4f2c59e90098695e9c97ced3371815712c59d8af
alastifer <grishkevichaaa@gmail.com>
2016-08-16 13:54:31 +0300

    in class InputCoordinateController add exception InputMismatchException

--- src/project/xo/controller/InputCoordinateController.java
+++ src/project/xo/controller/InputCoordinateController.java
@@ -1,21 +1,19 @@
 package project.xo.controller;
 
-import project.xo.controller.exceptions.AlreadyOccupiedException;
 import project.xo.controller.exceptions.InvalidPointException;
-import project.xo.model.Field;
 import project.xo.model.Point;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputCoordinateController {
 
-    public Point enterCoordinate(final Field field) throws InvalidPointException {
-        int fieldSize = field.getSize();
+    public Point enterCoordinate(final int fieldSize) throws InvalidPointException, InputMismatchException {
         Point point = new Point(validCoordinate(fieldSize), validCoordinate(fieldSize));
         return point;
     }
 
-    private int validCoordinate(final int fieldSize) throws InvalidPointException{
+    private int validCoordinate(final int fieldSize) throws InvalidPointException, InputMismatchException {
         int n = new Scanner(System.in).nextInt();
 
         if (n < 0 || n >= fieldSize) {
