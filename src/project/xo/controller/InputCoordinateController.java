4cae9fc95ad6a2a685a0a4c53e3a0a67922f21f4
alastifer <grishkevichaaa@gmail.com>
2016-08-16 17:14:55 +0300

    refactor InputCoordinateController

--- src/project/xo/controller/InputCoordinateController.java
+++ src/project/xo/controller/InputCoordinateController.java
@@ -1,26 +1,24 @@
 package project.xo.controller;
 
 import project.xo.controller.exceptions.InvalidPointException;
-import project.xo.model.Point;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputCoordinateController {
 
-    public Point enterCoordinate(final int fieldSize) throws InvalidPointException, InputMismatchException {
-        Point point = new Point(validCoordinate(fieldSize), validCoordinate(fieldSize));
-        return point;
-    }
-
-    private int validCoordinate(final int fieldSize) throws InvalidPointException, InputMismatchException {
+    public int enterCoordinate(final int fieldSize) throws InvalidPointException, InputMismatchException {
         int n = new Scanner(System.in).nextInt();
 
+        validCoordinate(n, fieldSize);
+
+        return n;
+    }
+
+    private void validCoordinate(final int n, final int fieldSize) throws InvalidPointException {
         if (n < 0 || n >= fieldSize) {
             throw new InvalidPointException();
         }
-
-        return n;
     }
 
 }
