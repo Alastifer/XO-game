70976fe144da20380b5f63830c7906e61a443e52
alastifer <grishkevichaaa@gmail.com>
2016-08-17 16:17:33 +0300

    refactor WinnerControllerTest(view field with winner figure)

--- tst/project/xo/controller/WinnerControllerTest.java
+++ tst/project/xo/controller/WinnerControllerTest.java
@@ -1,6 +1,7 @@
 package project.xo.controller;
 
 import org.junit.Test;
+import project.xo.controller.exceptions.AlreadyOccupiedException;
 import project.xo.model.Field;
 import project.xo.model.Figure;
 import project.xo.model.Point;
@@ -11,50 +12,71 @@ public class WinnerControllerTest {
     @Test
     public void getWinner() throws Exception {
         Field field = new Field(3);
+        Figure figure = choice(field);
+        Random random = new Random();
+        int x, y;
+        int fieldSize = field.getSize();
 
-        for (int i = 0; i < field.getSize(); i++) {
-            for (int j = 0; j < field.getSize(); j++) {
-                field.setFigure(choice(), new Point(j, i));
-                System.out.print(field.getFigure(new Point(j, i)) + " ");
+        while (figure != null) {
+            x = random.nextInt(fieldSize);
+            y = random.nextInt(fieldSize);
+
+            try {
+                new MoveController().applyFigure(field, new Point(x, y), figure);
+            } catch (AlreadyOccupiedException e) {
+                continue;
             }
 
-            System.out.println();
+            figure = choice(field);
         }
 
-        /*field.setFigure(Figure.X, new Point(0, 0));
-        field.setFigure(Figure.O, new Point(1, 0));
-        field.setFigure(Figure.O, new Point(2, 0));
-
-        field.setFigure(Figure.X, new Point(0, 1));
-        field.setFigure(Figure.O, new Point(1, 1));
-        field.setFigure(Figure.X, new Point(2, 1));
-
-        field.setFigure(Figure.O, new Point(0, 2));
-        field.setFigure(Figure.X, new Point(1, 2));
-        field.setFigure(Figure.O, new Point(2, 2));
-
-        for (int i = 0; i < field.getSize(); i++) {
-            for (int j = 0; j < field.getSize(); j++) {
-                System.out.print(field.getFigure(new Point(j, i)) + " ");
-            }
-
-            System.out.println();
-        }*/
+        fieldView(field);
 
         WinnerController winnerController = new WinnerController();
 
-        System.out.println(winnerController.getWinner(field));
+        System.out.printf("\nWinner is figure: %s\n", winnerController.getWinner(field));
 
     }
 
-    private Figure choice() {
-        int randInt = new Random().nextInt(2);
+    private void fieldView(final Field field) {
+        int fieldSize = field.getSize();
+        Figure figure;
+        String delimiter = "|";
 
-        if (randInt == 1) {
-            return Figure.X;
-        } else {
-            return  Figure.O;
+        for (int i = 0; i < fieldSize; i++) {
+            for (int j = 0; j < fieldSize; j++) {
+                figure = field.getFigure(new Point(j, i));
+                System.out.print(" " + (figure == null ? " " : figure)  + " ");
+
+                if (j < fieldSize - 1) {
+                    System.out.print(delimiter);
+                }
+            }
+
+            if (i < fieldSize - 1) {
+                fieldLine(fieldSize);
+            }
         }
+
+        System.out.printf("\n\n");
+
+    }
+
+    private void fieldLine(final int fieldSize) {
+        String line = "-";
+        int numOfLine = fieldSize * 3 + (fieldSize - 1);
+
+        System.out.println();
+
+        for (int i = 0; i < numOfLine; i++) {
+            System.out.print(line);
+        }
+
+        System.out.println();
+    }
+
+    private Figure choice(final Field field) {
+        return new CurrentMoveController().currentMove(field);
     }
 
 }
