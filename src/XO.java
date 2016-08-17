29635c9f5b0b6af3e563e20ef26d92ec0c3391f1
alastifer <grishkevichaaa@gmail.com>
2016-08-17 09:02:26 +0300

    Main class XO - created

---
+++ src/XO.java
@@ -0,0 +1,16 @@
+import project.xo.model.Game;
+import project.xo.view.ConsoleView;
+import project.xo.view.GameCreateView;
+
+public class XO {
+
+    public static void main(String[] args) {
+        boolean gameController = true;
+        Game newGame = new GameCreateView().createView();
+        
+        while (gameController) {
+            gameController = new ConsoleView().gameView(newGame);
+        }
+    }
+
+}
