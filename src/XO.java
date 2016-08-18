a3eba8b8e92f5cc26d52a9bce738697a55d144b2
alastifer <grishkevichaaa@gmail.com>
2016-08-18 11:19:11 +0300

    add throws IOException and InterruptedException

--- src/XO.java
+++ src/XO.java
@@ -2,9 +2,11 @@ import project.xo.model.Game;
 import project.xo.view.ConsoleView;
 import project.xo.view.GameCreateView;
 
+import java.io.IOException;
+
 public class XO {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         boolean gameController = true;
         Game newGame = new GameCreateView().createView();
         
