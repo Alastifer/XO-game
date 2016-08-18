1e7f93c3c2dff1a98b9d459b0363d3bdfecb43dd
alastifer <grishkevichaaa@gmail.com>
2016-08-18 20:03:58 +0300

    remove license

--- src/XO.java
+++ src/XO.java
@@ -1,6 +1,3 @@
-// Copyright 2016 Alexandr
-// Licensed under the Apache License, Version 2.0
-
 import project.xo.model.Game;
 import project.xo.view.ConsoleView;
 import project.xo.view.GameCreateView;
