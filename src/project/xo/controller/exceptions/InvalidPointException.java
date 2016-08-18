e9359c88ef299d160a0548a6b878ac0f5840c19d
alastifer <grishkevichaaa@gmail.com>
2016-08-18 20:03:48 +0300

    remove license

--- src/project/xo/controller/exceptions/InvalidPointException.java
+++ src/project/xo/controller/exceptions/InvalidPointException.java
@@ -1,6 +1,3 @@
-// Copyright 2016 Alexandr
-// Licensed under the Apache License, Version 2.0
-
 package project.xo.controller.exceptions;
 
 public class InvalidPointException extends AbstractXOException {
--- src/project/xo/view/GameCreateView.java
+++ src/project/xo/view/GameCreateView.java
@@ -1,6 +1,3 @@
-// Copyright 2016 Alexandr
-// Licensed under the Apache License, Version 2.0
-
 package project.xo.view;
 
 import project.xo.controller.GameCreateController;
