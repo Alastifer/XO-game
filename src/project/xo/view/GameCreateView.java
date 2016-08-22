8ce39786507f4bf9f005b219f3fe6c146f44f148
alastifer <grishkevichaaa@gmail.com>
2016-08-22 19:06:54 +0300

    refactoring GameCreateView (delete class GameCreateController and his test,  instance of class Game create in class CameCreateView)

--- src/project/xo/view/GameCreateView.java
+++ src/project/xo/view/GameCreateView.java
@@ -1,8 +1,10 @@
 package project.xo.view;
 
-import project.xo.controller.GameCreateController;
 import project.xo.controller.exceptions.FieldSizeException;
+import project.xo.model.Field;
+import project.xo.model.Figure;
 import project.xo.model.Game;
+import project.xo.model.Player;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -15,12 +17,17 @@ public class GameCreateView {
     public Game createView() {
         ClearConsoleView.clearConsole();
         helloMessage();
-        final int fieldSize = fieldSizeCreate();
-        final String playerXName = playerXCreate();
-        final String playerOName = playerOCreate();
+        final String gameName = "XO-game";
+        final Field field = fieldCreate();
+        final Player playerX = playerXCreate();
+        final Player playerO = playerOCreate();
         ClearConsoleView.clearConsole();
-        return new GameCreateController().gameCreate(fieldSize, playerXName, playerOName);
-
+        return new Game.Builder()
+                .gameName(gameName)
+                .field(field)
+                .playerX(playerX)
+                .playerO(playerO)
+                .build();
     }
 
     private void helloMessage() {
@@ -28,7 +35,7 @@ public class GameCreateView {
         System.out.println(helloMessage);
     }
 
-    private int fieldSizeCreate() {
+    private Field fieldCreate() {
         final int fieldSize;
         final String enterMessage = "Enter size of the game field: ";
 
@@ -40,14 +47,14 @@ public class GameCreateView {
         } catch (InputMismatchException e) {
             final String errorMessage = String.format("Invalid performance of field size! Size is number from %d to %d", MIN, MAX);
             System.out.println(errorMessage);
-            return fieldSizeCreate();
+            return fieldCreate();
         } catch (FieldSizeException e) {
             final String errorMessage = String.format("Invalid size of field! Enter value from %d to %d", MIN, MAX);
             System.out.println(errorMessage);
-            return fieldSizeCreate();
+            return fieldCreate();
         }
 
-        return fieldSize;
+        return new Field(fieldSize);
     }
 
     private void sizeCheck(final int fieldSize) throws FieldSizeException {
@@ -56,17 +63,18 @@ public class GameCreateView {
         }
     }
 
-    private String playerXCreate() {
+    private Player playerXCreate() {
         final String enterMessage = "Enter name for player, who will play figure X: ";
         System.out.print(enterMessage);
-        return new Scanner(System.in).nextLine();
+        final String name = new Scanner(System.in).nextLine();
+        return new Player(name, Figure.X);
     }
 
-    private String playerOCreate() {
+    private Player playerOCreate() {
         final String enterMessage = "Enter name for player, who will play figure O: ";
         System.out.print(enterMessage);
-        return new Scanner(System.in).nextLine();
-
+        final String name = new Scanner(System.in).nextLine();
+        return new Player(name, Figure.O);
     }
 
 }
